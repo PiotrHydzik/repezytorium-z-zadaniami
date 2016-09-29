2b78f6bff92201d37de498f694c16c3d2c9dc4fd
Piotr Hydzik <hydzik.p@gmai.com>
2016-09-29 23:20:00 +0200

    Wszystkie dane wprowadzone

--- KRA_FRO_W_02_JavaScript_prework-master/1_Pierwszy_skrypt/script.js
+++
@@ -1,8 +0,0 @@
-
-console.log("Piotrek");
-
-/* Po działaniach za każdym razem po wciśnięciu enter ukazywał się dobry wynik*/
-
-/* Po "Hello World" został wypisanY normalnie tekst */
-/* Po Hello World; został wypisanY komunikat Uncaught SyntaxError: Unexpected identifier */
-/* Po "Hello World; został wypisanY komunikat Uncaught SyntaxError: Invalid or unexpected token */
