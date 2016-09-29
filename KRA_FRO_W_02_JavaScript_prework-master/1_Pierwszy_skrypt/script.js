2718d2f7bc9c884f227539846835e1de9bed4017
Piotr Hydzik <hydzik.p@gmai.com>
2016-09-29 22:29:35 +0200

    Opisane w komentaru wszystkie dzialania z konsoli

---
+++ KRA_FRO_W_02_JavaScript_prework-master/1_Pierwszy_skrypt/script.js
@@ -0,0 +1,8 @@
+
+console.log("Piotrek");
+
+/* Po działaniach za każdym razem po wciśnięciu enter ukazywał się dobry wynik*/
+
+/* Po "Hello World" został wypisanY normalnie tekst */
+/* Po Hello World; został wypisanY komunikat Uncaught SyntaxError: Unexpected identifier */
+/* Po "Hello World; został wypisanY komunikat Uncaught SyntaxError: Invalid or unexpected token */
